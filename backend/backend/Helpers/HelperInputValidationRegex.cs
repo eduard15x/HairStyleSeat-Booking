df65ecc2e5689935435bc49c291f90f1f30b2b7c
Eduard Precup <precupeduard99@gmail.com>
2023-10-17 19:47:51 +0300

    Create RegExr to check time string format and helper function to check if time stay withing interval

--- backend/backend/Helpers/HelperInputValidationRegex.cs
+++ backend/backend/Helpers/HelperInputValidationRegex.cs
@@ -5,10 +5,16 @@ namespace backend.Helpers
     public class HelperInputValidationRegex
     {
         private readonly string WorkDaysForSalon = "^(monday|tuesday|wednesday|thursday|friday|saturday|sunday)(,(monday|tuesday|wednesday|thursday|friday|saturday|sunday))*$";
+        private readonly string ValidTimeFormat = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
 
         protected bool CheckWorkDaysInput(string input)
         {
             return Regex.IsMatch(input, WorkDaysForSalon);
+        }
+
+        protected bool CheckValidTimeFormat(string input)
+        {
+            return Regex.IsMatch(input, ValidTimeFormat);
         }
     }
 }
---
+++ backend/backend/Helpers/HelperTimeValidation.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace backend.Helpers
+{
+    public class HelperTimeValidation
+    {
+        protected bool CheckHourWithinInterval(string selectedHour, string startTime, string endTime)
+        {
+            DateTime userTime = DateTime.ParseExact(selectedHour, "HH:mm", CultureInfo.InvariantCulture);
+            DateTime startTimeObj = DateTime.ParseExact(startTime, "HH:mm", CultureInfo.InvariantCulture);
+            DateTime endTimeObj = DateTime.ParseExact(endTime, "HH:mm", CultureInfo.InvariantCulture);
+
+            if (userTime.TimeOfDay >= startTimeObj.TimeOfDay && userTime.TimeOfDay <= endTimeObj.TimeOfDay)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //Note that the "HH:mm" format string specifies that the input will be in 24-hour format with a leading zero for single-digit hours.
+            //If your input uses a different format, you'll need to adjust the format string accordingly.
+        }
+    }
+}
