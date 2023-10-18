c8fa6e4df4c8023fb41809d8d725ae0e6f7110b9
Eduard Precup <precupeduard99@gmail.com>
2023-10-18 17:24:49 +0300

    Rename Helper class and create a new method for validate input for reservation
    CheckReservationDateAndTime method
    new RegExr pattern ValidDateFormat

--- backend/backend/Helpers/HelperDateTimeValidation.cs
+++ backend/backend/Helpers/HelperDateTimeValidation.cs
@@ -1,8 +1,9 @@
-﻿using System.Globalization;
+﻿using backend.Dtos.Reservation;
+using System.Globalization;
 
 namespace backend.Helpers
 {
-    public class HelperTimeValidation
+    public class HelperDateTimeValidation
     {
         public static bool CheckHourInterval(string startTime, string endTime)
         {
@@ -39,6 +40,30 @@ namespace backend.Helpers
             }
             //Note that the "HH:mm" format string specifies that the input will be in 24-hour format with a leading zero for single-digit hours.
             //If your input uses a different format, you'll need to adjust the format string accordingly.
+        }
+
+        public static bool CheckReservationDateAndTime(string reservationDay, string reservationHour)
+        {
+            if (!HelperInputValidationRegex.CheckValidDateFormat(reservationDay))
+                throw new Exception("Date format is invalid, respect format: \"MM/dd/yyyy\".");
+
+            if (!HelperInputValidationRegex.CheckValidTimeFormat(reservationHour))
+                throw new Exception("Date format is invalid, respect format: \"15:35\".");
+
+            var reservationDayObj = DateTime.Parse(reservationDay);
+
+            // TimeZona For Romania
+            TimeZoneInfo targetTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GTB Standard Time");
+            // Get the current time in the target time zone
+            DateTime currentDateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, targetTimeZone);
+
+            if (reservationDayObj < currentDateTime)
+                throw new Exception("Reservation day should be in the future.");
+
+            if (reservationDayObj.ToString("MM/dd/yyyy") == currentDateTime.ToString("MM/dd/yyyy"))
+                throw new Exception("You can not make a reservation in the same day.");
+
+            return true;
         }
     }
 }
--- backend/backend/Helpers/HelperInputValidationRegex.cs
+++ backend/backend/Helpers/HelperInputValidationRegex.cs
@@ -4,8 +4,9 @@ namespace backend.Helpers
 {
     public class HelperInputValidationRegex
     {
-        private static readonly string WorkDaysForSalon = "^(monday|tuesday|wednesday|thursday|friday|saturday|sunday)(,(monday|tuesday|wednesday|thursday|friday|saturday|sunday))*$";
-        private static readonly string ValidTimeFormat = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
+        private const string WorkDaysForSalon = "^(monday|tuesday|wednesday|thursday|friday|saturday|sunday)(,(monday|tuesday|wednesday|thursday|friday|saturday|sunday))*$";
+        private const string ValidTimeFormat = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
+        private const string ValidDateFormat = "^(0[1-9]|1[012])[-/.]";
 
         public static bool CheckWorkDaysInput(string input)
         {
@@ -15,6 +16,11 @@ namespace backend.Helpers
         public static bool CheckValidTimeFormat(string input)
         {
             return Regex.IsMatch(input, ValidTimeFormat);
+        }
+
+        public static bool CheckValidDateFormat(string input)
+        {
+            return Regex.IsMatch(input, ValidDateFormat);
         }
     }
 }
