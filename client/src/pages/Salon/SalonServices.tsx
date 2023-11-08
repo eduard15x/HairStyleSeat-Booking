c65f6c7a51c89b094a9670fe65010855920bc5cc
Precup Eduard <precupeduard99@gmail.com>
2023-11-08 02:12:01 +0200

    Create business logic and request on Frontend for SalonReservations & SalonServices

--- client/src/pages/Salon/SalonServices.tsx
+++ client/src/pages/Salon/SalonServices.tsx
@@ -1,5 +1,105 @@
+import { useState, useEffect } from "react";
+import { useUserContext } from "../../hooks/useUserContext";
+
+const GET_SALON_RESERVATION_LIST_URL_STRING = process.env.REACT_APP_SALON_RESERVATION_LIST_URL;
+const GET_SALON_DETAILS_FOR_USER = process.env.REACT_APP_GET_SALON_DETAILS_FOR_USER_URL;
+const GET_SALON_SERVICES_LIST_URL_STRING = process.env.REACT_APP_SALON_SERVICES_LIST_URL;
+
+
+const strings = "?salonAffiliateId=2"
+
+
+
+// ${SALON_SERVICES_LIST_URL_STRING}${salonId}/services`
+
+
 export const SalonServices = () => {
+  const { userState } = useUserContext();
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<any>(null);
+  const [el, setEl] = useState<number>(0);
+
+
+  const getSalonServices = async () => {
+    setError(null);
+    setIsLoading(true);
+    setEl(0);
+
+    try {
+      const response1 = await fetch(GET_SALON_DETAILS_FOR_USER!, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+        credentials: "include"
+      });
+      const json1 = await response1.json();
+
+      if (json1.statusCode >= 400) {
+        setError(json1.value);
+        return;
+      } else if (json1.value === null) {
+        return;
+      }
+
+      const response2 = await fetch(`${GET_SALON_SERVICES_LIST_URL_STRING}${json1.value.salonId}/services`, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+        credentials: "include"
+      });
+      const json2 = await response2.json();
+
+      console.log(response2);
+      console.log(json2);
+      
+      if (json2.statusCode >= 400) {
+        setError(json2.value);
+        console.error(json2.value);
+      } else {
+        setEl(json2.value.length);
+      }
+
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+  useEffect(() => {
+    getSalonServices();
+  }, []);
+
+  const loadingAnimation =
+  <div className='flex items-center justify-center m-auto h-full'>
+    <svg
+        aria-hidden="true"
+        className="w-8 h-8 text-gray-100 animate-spin dark:text-gray-600 fill-blue-500 "
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"/>
+        <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"/>
+    </svg>
+  </div>
+
+
+  const renderedElement = el !== 0 ? <p>This is ok, multiple values</p> : (error !== null ? <p>{error}</p> : <p>No values, 0 lengt</p>);
+
+
   return (
-    <div>SalonServices</div>
+    <div className='w-full min-h-[calc(100vh-100px-50px)] text-gray-200'>
+      <h1 className='text-gray-200 text-2xl tablet:text-2xl text-center pb-12 mt-4'>Salon Services</h1>
+
+      {
+        isLoading
+        ? loadingAnimation
+        : renderedElement
+      }
+
+    </div>
   );
 };
